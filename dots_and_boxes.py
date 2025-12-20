7005fdb0ce13e0408469d1553d9f60d80a761c87
tovi21 <tovjanina@gmail.com>
2025-12-20 14:42:53 +0100

    get_actions and get_successors

--- dots_and_boxes.py
+++ dots_and_boxes.py
@@ -1,4 +1,4 @@
-
+from copy import deepcopy
 
 class DotsAndBoxes:
 
@@ -7,9 +7,9 @@ class DotsAndBoxes:
         self.state = {
             'board' : [['o' if j % 2 == 0 
                         else ' ' 
-                        for j in range(n*2)] if i % 2 == 0
-                       else [' ' for j in range(n*2)] 
-                       for i in range(n*2)],
+                        for j in range(n*2-1)] if i % 2 == 0
+                       else [' ' for j in range(n*2-1)] 
+                       for i in range(n*2-1)],
             'player' : 'A'
         }
 
@@ -17,3 +17,46 @@ class DotsAndBoxes:
         for row in state['board']:
             print(' '.join(row))
         print()
+
+    def get_actions(self, state):
+        actions = []
+        for i in range(self.n*2-1):
+            for j in range(self.n*2-1):
+                if i % 2 == 0:
+                    if j % 2 != 0:
+                        if state['board'][i][j] == ' ':
+                            actions.append((i, j, state['player'], '-'))
+                else:
+                    if j % 2 == 0:
+                        if state['board'][i][j] == ' ':
+                            actions.append((i, j, state['player'], '|'))
+        return actions
+    
+    def get_successor(self, state, action):
+        new_state = deepcopy(state)
+        i, j, player, line = action
+
+        new_state['board'][i][j] = line
+
+        # provjera da li je zatvorena kocka
+        if line == '-':
+            # gornja kocka
+            if i-1 >= 0 and state['board'][i-2][j] == '-' and state['board'][i-1][j-1] == '|' and state['board'][i-1][j+1] == '|':
+                new_state['board'][i-1][j] = state['player']
+            # donja kocka
+            elif i+1 <= self.n-1 and state['board'][i+2][j] == '-' and state['board'][i+1][j-1] == '|' and state['board'][i+1][j+1] == '|':
+                new_state['board'][i+1][j] = state['player']
+            else:
+                new_state['player'] = 'B' if player == 'A' else 'A'
+        elif line == '|':
+            # lijeva kocka
+            if j >= 0 and state['board'][i][j-2] == '|' and state['board'][i-1][j-1] == '-' and state['board'][i+1][j-1] == '-':
+                new_state['board'][i][j-1] = state['player']
+            # desna kocka
+            elif j <= self.n-1 and state['board'][i][j+2] == '|' and state['board'][i-1][j+1] == '-' and state['board'][i+1][j+1] == '-':
+                new_state['board'][i][j+1] = state['player']
+            else:
+                new_state['player'] = 'B' if player == 'A' else 'A'
+
+        return new_state
+    
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 from dots_and_boxes import DotsAndBoxes
 
 n = int(input('Velicina tabele: '))
@@ -5,4 +6,25 @@ game = DotsAndBoxes(n)
 
 state = game.state
 
-game.print_state(state)
+# game.print_state(state)
+# print(game.get_actions(state))
+
+# action = random.choice(game.get_actions(state))
+# print(action)
+# new_state = game.get_successor(state, action)
+# game.print_state(new_state)
+
+# action1 = random.choice(game.get_actions(new_state))
+# print(action1)
+# new_state1 = game.get_successor(new_state, action1)
+# game.print_state(new_state1)
+
+# action2 = random.choice(game.get_actions(new_state1))
+# print(action2)
+# new_state2 = game.get_successor(new_state1, action2)
+# game.print_state(new_state2)
+
+# action3 = random.choice(game.get_actions(new_state2))
+# print(action3)
+# new_state3 = game.get_successor(new_state2, action3)
+# game.print_state(new_state3)
