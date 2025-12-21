7e37ef4dfdad22397ed6eacb6dfbf930bc02a388
tovi21 <tovjanina@gmail.com>
2025-12-21 23:18:18 +0100

    added score tracking and printing

--- dots_and_boxes.py
+++ dots_and_boxes.py
@@ -4,6 +4,8 @@ class DotsAndBoxes:
 
     def __init__(self, n):
         self.n = n
+        self.A_score = 0
+        self.B_score = 0
         self.state = {
             'board' : [['o' if j % 2 == 0 
                         else ' ' 
@@ -16,6 +18,8 @@ class DotsAndBoxes:
     def print_state(self, state):
         for row in state['board']:
             print(' '.join(row))
+        print('A score:', self.A_score)
+        print('B score:', self.B_score)
         print()
 
     def get_actions(self, state):
@@ -43,18 +47,34 @@ class DotsAndBoxes:
             # gornja kocka
             if i-1 >= 0 and state['board'][i-2][j] == '-' and state['board'][i-1][j-1] == '|' and state['board'][i-1][j+1] == '|':
                 new_state['board'][i-1][j] = state['player']
+                if state['player'] == 'A':
+                    self.A_score += 1
+                else:
+                    self.B_score += 1
             # donja kocka
             elif i+1 <= self.n-1 and state['board'][i+2][j] == '-' and state['board'][i+1][j-1] == '|' and state['board'][i+1][j+1] == '|':
                 new_state['board'][i+1][j] = state['player']
+                if state['player'] == 'A':
+                    self.A_score += 1
+                else:
+                    self.B_score += 1
             else:
                 new_state['player'] = 'B' if player == 'A' else 'A'
         elif line == '|':
             # lijeva kocka
             if j >= 0 and state['board'][i][j-2] == '|' and state['board'][i-1][j-1] == '-' and state['board'][i+1][j-1] == '-':
                 new_state['board'][i][j-1] = state['player']
+                if state['player'] == 'A':
+                    self.A_score += 1
+                else:
+                    self.B_score += 1
             # desna kocka
             elif j <= self.n-1 and state['board'][i][j+2] == '|' and state['board'][i-1][j+1] == '-' and state['board'][i+1][j+1] == '-':
                 new_state['board'][i][j+1] = state['player']
+                if state['player'] == 'A':
+                    self.A_score += 1
+                else:
+                    self.B_score += 1
             else:
                 new_state['player'] = 'B' if player == 'A' else 'A'
 
